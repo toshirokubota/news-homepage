4ad8c076e6decc00cbc339a07743995cff2fd913
Toshiro Kubota <toshiro732@hotmail.com>
2025-06-01 09:30:44 -0400

    almost done

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,18 @@ const navbar = document.getElementById('navbar');
 nav_icon?.addEventListener('click', () => {
     navbar?.classList.toggle('hidden');
     navbar?.setAttribute("aria-expanded", "true");
+    nav_icon.classList.toggle('hidden');
+    nav_close_icon?.classList.toggle('hidden');
 })
 nav_close_icon?.addEventListener('click', () => {
     navbar?.classList.toggle('hidden');
     navbar?.setAttribute("aria-expanded", "false");
+    nav_icon?.classList.toggle('hidden');
+    nav_close_icon.classList.toggle('hidden');
 })
 window.addEventListener("resize", () => {
     if (window.innerWidth >= 800) {
-        navbar?.setAttribute("aria-expanded", "true"); // Correct usage
+        navbar?.setAttribute("aria-expanded", "true"); 
     } else {
         navbar?.setAttribute("aria-expanded", "false");
     }
